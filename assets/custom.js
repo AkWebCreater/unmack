8388683028352cb67c5c2ed0d16b43fb797d3236
Akash Sager <ak.web.creater@gmail.com>
2021-09-02 13:00:49 +0530

    css style changed and made new custome js file and our-journeycss

--- assets/custom.js
+++ assets/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
    $('.year').click(function(){
     var index = $(this).attr('data-index');
-    $('.year-img[data-index='+index+']').addClass('active')
+    $('.year-img[data-index='+index+']').addClass('active').siblings().removeClass('active')
    })
 })
