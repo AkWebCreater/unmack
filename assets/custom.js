b72b71ecd061f6d29de5613c4f6e9338aa7d4631
Akash Sager <ak.web.creater@gmail.com>
2021-09-25 17:22:14 +0530

    conatct form respositioneted

--- assets/custom.js
+++ assets/custom.js
@@ -58,12 +58,12 @@ $(document).on('click','#bulk-add-button',function(){
 
     $('.card-btn').click(function(e){
       e.preventDefault();
-      $(this).closest('.bottom-card').find('.popup-layer').addClass('open');
+      $('.popup-layer').addClass('open');
       $('body').addClass('scroll-block')
     })
     $(document).on('click','.overlay',function(e){
       
-      $(this).closest('.popup-layer').removeClass('open');
+      $('.popup-layer').removeClass('open');
       $('body').removeClass('scroll-block');
     })
   })
