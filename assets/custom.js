4a19b0a1b19348c28b52bcc331db7165d877a3a8
Akash Sager <ak.web.creater@gmail.com>
2021-09-28 16:11:52 +0530

    faq address block added

--- assets/custom.js
+++ assets/custom.js
@@ -72,5 +72,20 @@ $(document).on('click','#bulk-add-button',function(){
 $(this).next().slideToggle()
 $(this).toggleClass('open')
     })
-    
+    // map on faq page
+    // Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
   })
