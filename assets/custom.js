fa4ff1de42f98d143cdc689d7095b04025890e32
Akash Sager <ak.web.creater@gmail.com>
2021-11-19 15:20:39 +0530

    home best seller slider not working fixed again-3

--- assets/custom.js
+++ assets/custom.js
@@ -170,8 +170,7 @@ $(document).ready(function() {
    });
 $('.slick-arrow').on('click',function(){
   var currentIndex = $('.slick-current').attr('data-index');
-  alert(currentIndex)
-  $('.best-seller-nav span[data-indez'+ currentIndex +']').addClass('add-border').siblings().removeClass('add-border');
+  $('.best-seller-nav span[data-index'+ currentIndex +']').addClass('add-border').siblings().removeClass('add-border');
 })
   // website popup homepage
   if (localStorage.getItem("popup-web") === null) {
