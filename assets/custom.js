d04147bb5f5c09ddd31a4eecdb6b43d900d69ccf
Akash Sager <ak.web.creater@gmail.com>
2021-11-24 14:40:38 +0530

    home  page product range slider fixed-9

--- assets/custom.js
+++ assets/custom.js
@@ -32,17 +32,25 @@ $('.product-range-slider ul').slick({
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 750,
+      breakpoint:1024,
       settings: {
         slidesToShow: 4,
-        slidesToScroll: 2,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+      },
+    },
+    {
+      breakpoint: 750,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
         adaptiveHeight: true,
       },
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
