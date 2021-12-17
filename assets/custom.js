8f568f52227ed405ddd7d8627137bedf90f8e6f5
Akash Sager <ak.web.creater@gmail.com>
2021-12-17 10:22:03 +0530

    cart footer  fixing 147

--- assets/custom.js
+++ assets/custom.js
@@ -586,30 +586,32 @@ $('.custome-select').each(function(){
 });
 
 // countdown timer for deals of day collection page
-(function() {
-  const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-  var offerTime = document.getElementById("offer-time").innerText
-  const countDown = new Date(offerTime).getTime(),
-      x = setInterval(function() {
-
-          const now = new Date().getTime(),
-              distance = countDown - now;
-
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
-              document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-              document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-              document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-          //do something later when date is reached
-          if (distance < 0) {
-              document.getElementById("headline").innerText = "Offer Gone";
-              document.getElementById("countdown").style.display = "none";
-              document.getElementById("content").style.display = "block";
-              clearInterval(x);
-          }
-          //seconds
-      }, 0)
-}());
+if($('.body--collection.deal-of-day ').length){
+    (function() {
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+        var offerTime = document.getElementById("offer-time").innerText
+        const countDown = new Date(offerTime).getTime(),
+            x = setInterval(function() {
+      
+                const now = new Date().getTime(),
+                    distance = countDown - now;
+      
+                document.getElementById("days").innerText = Math.floor(distance / (day)),
+                    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+                    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+                    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+      
+                //do something later when date is reached
+                if (distance < 0) {
+                    document.getElementById("headline").innerText = "Offer Gone";
+                    document.getElementById("countdown").style.display = "none";
+                    document.getElementById("content").style.display = "block";
+                    clearInterval(x);
+                }
+                //seconds
+            }, 0)
+      }());
+}
